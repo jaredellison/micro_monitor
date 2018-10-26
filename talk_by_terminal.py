d026790965ba4123c017ae159c3ca1d1efc35be1
Jared Ellison <jaredellison@gmail.com>
2018-10-25 23:02:40 -0400

    Add resize_event function and comments to talk_by_terminal.py

--- talk_by_terminal.py
+++ talk_by_terminal.py
@@ -1,3 +1,11 @@
+#! /usr/bin/python
+'''
+    File name: talk_by_terminal.py
+    Author: Jared Ellison
+    Date created: Oct 2018
+    Python Version: 3.7
+'''
+
 import curses
 import math
 import time
@@ -9,6 +17,7 @@ import time
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 # Curses helper function from http://devcry.heiho.net/html/2016/20160228-curses-practices.html
+# This helps identify when escape key is pressed to exit the program
 def getch():
   KEY_TABLE = {'0x1b': 'ESC'}
   '''Returns keystroke as string'''
@@ -34,6 +43,12 @@ def getch():
   # unknown key; just return as a hex string
   return skey
 
+def resize_event():
+    y, x = screen.getmaxyx()
+    screen.clear()
+    curses.resizeterm(y, x)
+    screen.refresh()
+
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 screen = curses.initscr()
@@ -43,6 +58,13 @@ receivedBuffer = ['message 1', 'message 2', 'message 3', 'message 4', 'message 5
 
 # Press escape key to exit
 q = ''
+
+
+##############################
+#                            #
+#         Main Loop          #
+#                            #
+##############################
 
 while q != 'ESC':
   screen.clear()
@@ -64,6 +86,14 @@ while q != 'ESC':
 
   q = getch()
   screen.refresh()
+
+
+##############################
+#                            #
+#      Exit and Clean Up     #
+#                            #
+##############################
+
 
 # Clear screen and exit
 screen.clear()

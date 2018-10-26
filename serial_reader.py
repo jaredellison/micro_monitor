97fdbabb49414606a04dfc4310cb8ebd2654d2c8
Jared Ellison <jaredellison@gmail.com>
2018-10-26 10:28:30 -0400

    Add ReadLine class to deal with readline timeouts

--- serial_reader.py
+++ serial_reader.py
@@ -33,6 +33,29 @@ def receive_message():
     print(line.decode('utf-8').strip())
   return
 
+# Class from https://github.com/pyserial/pyserial/issues/216
+class ReadLine:
+    def __init__(self, s):
+        self.buf = bytearray()
+        self.s = s
+        print(s)
+
+    def readline(self):
+        i = self.buf.find(b"\n")
+        if i >= 0:
+            r = self.buf[:i+1]
+            self.buf = self.buf[i+1:]
+            return r
+        while True:
+            i = max(1, min(2048, self.s.in_waiting))
+            data = self.s.read(i)
+            i = data.find(b"\n")
+            if i >= 0:
+                r = self.buf + data[:i+1]
+                self.buf[0:] = data[i+1:]
+                return r
+            else:
+                self.buf.extend(data)
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
@@ -87,5 +110,9 @@ else:
     else:
       print('Please enter a valid port number')
 
+reader = ReadLine(ser)
+
 while True:
-  receive_message()
+  # print(ser.in_waiting)
+  # receive_message()
+  print(reader.readline().decode('utf-8').strip())

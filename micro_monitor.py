cd5e40b4ff4753008c65a1b38672f2b2132965c7
Jared Ellison <jaredellison@gmail.com>
2018-10-27 18:45:40 -0400

    Refactor into function blocks

--- micro_monitor.py
+++ micro_monitor.py
@@ -56,18 +56,18 @@ def resize_event():
     screen.refresh()
 
 
-def draw_section_dividers(division_point):
+def draw_section_dividers(division_point, dimensions):
     # Create section dividers to separate the screen
-    send_divider = '◦ send:      ' + '─' * (dims['x'] - 13)
-    receive_divider = '◦ receive:   ' + '─' * (dims['x'] - 13)
+    send_divider = '◦ send:      ' + '─' * (dimensions['x'] - 13)
+    receive_divider = '◦ receive:   ' + '─' * (dimensions['x'] - 13)
     screen.addstr(0, 0, send_divider, curses.color_pair(1))
     screen.addstr(division_point, 0, receive_divider, curses.color_pair(1))
 
 
-def draw_received(division_point):
+def draw_received(division_point, dimensions):
     # Draw messages received from serial port
-    receive_area = {'start': int(dims['y']/2 + 1),
-                    'lines': dims['y'] - division_point - 1}
+    receive_area = {'start': int(dimensions['y']/2 + 1),
+                    'lines': dimensions['y'] - division_point - 1}
 
     # Slice total received buffer down to the number
     # of messages that can be displayed
@@ -76,12 +76,12 @@ def draw_received(division_point):
         screen.addstr(division_point + 1 + i, 0, message)
 
 
-def assemble_to_send(char, send_message, sent_buffer):
+def assemble_to_send(serial_port, char, send_message, sent_buffer):
     # char is the ascii code for a character
     # New Line
     # if char == 10:
     if char in ('\n', 'Ctrl-J'):
-        serial_out(send_message)
+        serial_out(serial_port, send_message)
         sent_buffer.append(send_message)
         send_message = ''
 
@@ -96,10 +96,10 @@ def assemble_to_send(char, send_message, sent_buffer):
     return (send_message, sent_buffer)
 
 
-def draw_sent(division_point):
+def draw_sent(division_point, dimensions, send_message):
     # Draw messages received from serial port
     send_area = {'start': 1,
-                 'lines': dims['y'] - division_point - 3}
+                 'lines': dimensions['y'] - division_point - 3}
 
     # Slice total received buffer down to the number
     # of messages that can be displayed
@@ -120,7 +120,7 @@ def is_ascii(c):
     return len(c) == 1 and 31 < ord(c) < 128
 
 
-# A simple function to test if input strings are valid as integers
+# Test if input strings are valid as integers
 def is_int(s):
     try:
         int(s)
@@ -131,9 +131,9 @@ def is_int(s):
 
 # Text is input to the script as a string, here we add a new line character
 # and encode it to binary and send it to the open serial port.
-def serial_out(string):
+def serial_out(serial_port, string):
     string = string+'\n'
-    ser.write(string.encode())
+    serial_port.write(string.encode())
 
 
 def receive_message(serial_port):
@@ -146,7 +146,7 @@ def receive_message(serial_port):
     return
 
 
-def exit_gracefully(message=''):
+def exit_gracefully(screen, curses, message=''):
     # Clear screen and exit, possibly printing message
     curses.nocbreak()
     screen.keypad(0)
@@ -161,158 +161,175 @@ def exit_gracefully(message=''):
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-##############################
-#                            #
-#      Global Variables      #
-#                            #
-##############################
+#                     Core Functions
 
-send_message = ''
-sent_buffer = []
-inputLine = 0
-received_buffer = []
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-##############################
-#                            #
-#      Select Serial Port    #
-#                            #
-##############################
-
-
-print(""" .       __   __   __            __         ___  __   __
+def welcome():
+    print(""" .       __   __   __            __         ___  __   __
  |\\/| | /  ` |__) /  \\     |\\/| /  \\ |\\ | |  |  /  \\ |__)
  |  | | \\__, |  \\ \\__/ ___ |  | \\__/ | \\| |  |  \\__/ |  \\
-                                                          """)
-
-# search for available usb serial ports
-available_ports = [e for e in list_ports.grep('usb')]
-
-if len(available_ports) == 0:
-    print('No usb serial ports available, '
-          + 'please check that devices are connected.')
-    exit()
-elif len(available_ports) == 1:
-    print('One usb port available:')
-    selected_port = available_ports[0]
-    ser_send_path = selected_port.device
-    ser = serial.Serial(ser_send_path, 9600)
-    print(' -> Port selection: ' + selected_port.device
-          + ' ' + selected_port.manufacturer)
-    print(' -> Serial port connection opened at 9600 baud')
-else:
-    print('Please select a serial device:')
-    # Print a list of available devices
-    for i, port in enumerate(available_ports):
-        print('   {}. {} {}'.format(i + 1, port.device, port.manufacturer))
-    # Listen for a selection
-    while True:
-        selection = input('Port number: ')
-        # If it's a valid selection, open that port for communication
-        if is_int(selection) \
-           and int(selection) in range(1, len(available_ports) + 1):
-            print(' -> Port selection: ' + port.device
-                  + ' ' + port.manufacturer)
-            selected_port = available_ports[int(selection) - 1]
-            ser_send_path = selected_port.device
-            ser = serial.Serial(ser_send_path, 9600, timeout=.1)
-            print(' -> Serial port connection opened at 9600 baud')
-            break
-        else:
-            print('Please enter a valid port number')
+                                                             """)
 
 
-# Let users know how to quit
-print('\nPress escape key to exit at any time.'
-      + '\nPress return to enter serial monitor.')
+def open_serial_connection():
+    # search for available usb serial ports
+    available_ports = [e for e in list_ports.grep('usb')]
 
-while True:
-    key_input = getch()
-    # Check if escape key is pressed
-    if ord(key_input) == 27:
-        exit_gracefully()
+    if len(available_ports) == 0:
+        print('No usb serial ports available, '
+              + 'please check that devices are connected.')
+        exit()
+    elif len(available_ports) == 1:
+        print('One usb port available:')
+        selected_port = available_ports[0]
+        ser_send_path = selected_port.device
+        ser = serial.Serial(ser_send_path, 9600)
+        print(' -> Port selection: ' + selected_port.device
+              + ' ' + selected_port.manufacturer)
+        print(' -> Serial port connection opened at 9600 baud')
     else:
-        break
+        print('Please select a serial device:')
+        # Print a list of available devices
+        for i, port in enumerate(available_ports):
+            print('   {}. {} {}'.format(i + 1, port.device, port.manufacturer))
+        # Listen for a selection
+        while True:
+            selection = input('Port number: ')
+            # If it's a valid selection, open that port for communication
+            if is_int(selection) \
+               and int(selection) in range(1, len(available_ports) + 1):
+                print(' -> Port selection: ' + port.device
+                      + ' ' + port.manufacturer)
+                selected_port = available_ports[int(selection) - 1]
+                ser_send_path = selected_port.device
+                ser = serial.Serial(ser_send_path, 9600, timeout=.1)
+                print(' -> Serial port connection opened at 9600 baud')
+                break
+            else:
+                print('Please enter a valid port number')
 
-##############################
-#                            #
-#     Initialize Curses      #
-#                            #
-##############################
+    # Let users know how to quit
+    print('\nPress escape key to exit at any time.'
+          + '\nPress return to enter serial monitor.')
 
-screen = curses.initscr()
-
-# Don't show characters on the screen as they're typed. Characters are
-# shown using the assemble_to_send() function.
-curses.noecho()
-
-# The function curses_getch() blocks the cpu waiting for input so it's
-# important to set a timeout value for the screen. 250 Milliseconds is the
-# default. If the value is set too low, the script REALLY uses the CPU and
-# the screen may flicker but setting it lower does mean that incoming bytes
-# are displayed quicker.
-screen.timeout(250)
-
-curses.cbreak()
-screen.keypad(True)
-
-# Color Handling
-curses.start_color()
-curses.use_default_colors()
-
-# Set the input prompt cursor to blue
-# Init color pair is set with the arguments:
-#   id # starting at 1 (0 is the default)
-#   foreground color (-1 is the default color)
-#   background color (-1 is the default color)
-curses.init_pair(1, curses.COLOR_BLUE, -1)
-
-char_in = ''
-
-##############################
-#                            #
-#         Main Loop          #
-#                            #
-##############################
-
-# If any errors occur, curses leaves a mess in the terminal.
-# If any exeptions happen, exit gracefully.
-try:
     while True:
-        screen.clear()
-
-        # Get termianl dimensions
-        # Note that coordinates appear in the order y,x not x,y
-        dims = {'x': screen.getmaxyx()[1], 'y': screen.getmaxyx()[0]}
-        mid_y = math.floor(int(dims['y']/2))
-
-        draw_section_dividers(mid_y)
-
-        receive_message(ser)
-
-        draw_received(mid_y)
-
-        draw_sent(mid_y)
-
-        # Check for input characters
-        char_in = getch_curses()
-        if char_in == 'ESC':
+        key_input = getch()
+        # Check if escape key is pressed
+        if ord(key_input) == 27:
+            exit_gracefully()
+        else:
             break
+    return ser
 
-        send_message, sent_buffer = assemble_to_send(char_in,
-                                                     send_message,
-                                                     sent_buffer)
 
-        # Draw all changes to the screen
-        screen.refresh()
+def initilize_curses():
+    screen = curses.initscr()
 
-except OSError as e:
-    message = 'Exiting with error: ' + str(e)
-    exit_gracefully(message)
+    # Don't show characters on the screen as they're typed. Characters are
+    # shown using the assemble_to_send() function.
+    curses.noecho()
 
-##############################
-#                            #
-#      Exit and Clean Up     #
-#                            #
-##############################
+    # The function curses_getch() blocks the cpu waiting for input so it's
+    # important to set a timeout value for the screen. 250 Milliseconds is the
+    # default. If the value is set too low, the script REALLY uses the CPU and
+    # the screen may flicker but setting it lower does mean that incoming bytes
+    # are displayed quicker.
+    screen.timeout(250)
 
-exit_gracefully()
+    curses.cbreak()
+    screen.keypad(True)
+
+    # Color Handling
+    curses.start_color()
+    curses.use_default_colors()
+
+    # Set the input prompt cursor to blue
+    # Init color pair is set with the arguments:
+    #   id # starting at 1 (0 is the default)
+    #   foreground color (-1 is the default color)
+    #   background color (-1 is the default color)
+    curses.init_pair(1, curses.COLOR_BLUE, -1)
+
+    return screen
+
+
+def serial_monitor(serial_port, screen, sent_buffer,
+                   received_buffer, send_message):
+    # If any errors occur, curses leaves a mess in the terminal.
+    # If any exeptions happen, exit gracefully.
+    try:
+        while True:
+            screen.clear()
+
+            # Get termianl dimensions
+            # Note that coordinates appear in the order y,x not x,y
+            dims = {'x': screen.getmaxyx()[1], 'y': screen.getmaxyx()[0]}
+            mid_y = math.floor(int(dims['y']/2))
+
+            draw_section_dividers(mid_y, dims)
+
+            receive_message(serial_port)
+
+            draw_received(mid_y, dims)
+
+            draw_sent(mid_y, dims, send_message)
+
+            # Check for input characters
+            char_in = getch_curses()
+            if char_in == 'ESC':
+                break
+            elif char_in:
+                send_message, sent_buffer = assemble_to_send(serial_port,
+                                                             char_in,
+                                                             send_message,
+                                                             sent_buffer)
+
+            # Draw all changes to the screen
+            screen.refresh()
+
+    except Exception as e:
+        message = 'Exiting with error: ' + str(e)
+        exit_gracefully(screen, curses, message = message)
+
+
+if __name__ == '__main__':
+
+    ##############################
+    #                            #
+    #      Global Variables      #
+    #                            #
+    ##############################
+
+    send_message = ''
+    sent_buffer = []
+    received_buffer = []
+
+    welcome()
+
+    serial_port = open_serial_connection()
+
+    ##############################
+    #                            #
+    #     Initialize Curses      #
+    #                            #
+    ##############################
+
+    screen = initilize_curses()
+
+    ##############################
+    #                            #
+    #         Main Loop          #
+    #                            #
+    ##############################
+
+    serial_monitor(serial_port, screen, sent_buffer,
+                   received_buffer, send_message)
+
+    ##############################
+    #                            #
+    #      Exit and Clean Up     #
+    #                            #
+    ##############################
+
+    exit_gracefully()
